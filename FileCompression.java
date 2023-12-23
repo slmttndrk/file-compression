ecf53fe7ff4292fe78454d75e12c7dd8e9f186be
Selamettin Dirik <selamettinn.95@gmail.com>
2023-12-23 12:05:25 +0100

    Implemented a file read method to convert text into string.

--- FileCompression.java
+++ FileCompression.java
@@ -1,5 +1,18 @@
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class FileCompression {
-    public static void main(String[] args) {
-        System.out.println("Welcome, this is a file compression application!");
+    public static void main(String[] args) throws Exception {
+        System.out.println("Welcome, this is a file compression application! \n");
+
+        String dsa = readFileAsString("C:\\Users\\slmtt\\Desktop\\DSA-Projects.txt");
+        System.out.println(dsa);
+
+    }
+
+    // read method to convert text file into string
+    public static String readFileAsString(String filePath) throws Exception
+    {
+        return new String(Files.readAllBytes(Paths.get(filePath)));
     }
 }
