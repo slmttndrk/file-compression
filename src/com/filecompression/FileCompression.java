35dcca979c53aa441d9425919e1a0ca3b3f29b46
Selamettin Dirik <selamettinn.95@gmail.com>
2023-12-25 13:09:25 +0100

    Redundant parts are removed from FileCompression file.

--- src/com/filecompression/FileCompression.java
+++ src/com/filecompression/FileCompression.java
@@ -1,19 +1,13 @@
 package com.filecompression;
 
 import java.io.FileInputStream;
-import java.io.IOException;
 import java.io.ObjectInputStream;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.Base64;
 import java.util.HashMap;
-import java.util.PriorityQueue;
 
 public class FileCompression {
     public static void main(String[] args) throws Exception {
 
+        // check command line arguments
         if(args.length < 1)
         {
             System.out.println("Please give a file path as command line argument!");
@@ -28,8 +22,8 @@ public class FileCompression {
         // check if user wants to compress or decompress a file
         if (ap.getOption().equals("Compress"))
         {
-            // extract text from given file
-            String text = readUncompressedFile(ap);
+            // extract text from given filepath
+            String text = readUncompressedFile(ap.getFilePath());
 
             // create a map to store character-frequency pairs
             HashMap<Character, Integer> charFreqMap = new HashMap<>();
@@ -37,9 +31,6 @@ public class FileCompression {
             // fill character-frequency map for each of the characters
             for(Character c : text.toCharArray())
                 charFreqMap.put(c, charFreqMap.getOrDefault(c, 0) + 1);
-
-            System.out.println("2. charFreqMap : ");
-            charFreqMap.forEach((key, value) -> System.out.println(key + " -> " + value));
 
             // create a Huffman algorithm object to encode the text
             HuffmanStructure hs = new HuffmanStructure();
@@ -49,17 +40,13 @@ public class FileCompression {
         }
         else
         {
+            // extract huffman text and character-huffman map from given filepath
             Tuple<String, HashMap<Character, String>> tuple;
-            tuple = readCompressedFile(ap);
-
+            tuple = readCompressedFile(ap.getFilePath());
             String huffmanText = tuple.x;
             HashMap<Character, String> charHuffmanMap = tuple.y;
-            System.out.println("2. huffmanText : ");
-            System.out.println(huffmanText);
-            System.out.println("2. charHuffmanMap : ");
-            charHuffmanMap.forEach((k,v) -> System.out.println(k + " -> " + v));
 
-            // create a Huffman algorithm object to encode the text
+            // create a Huffman algorithm object to decode the text
             HuffmanStructure hs = new HuffmanStructure();
 
             // decode the huffman text
@@ -67,38 +54,30 @@ public class FileCompression {
         }
     }
 
-    private static String readUncompressedFile(ArgumentsParser ap)
+    private static String readUncompressedFile(String filePath)
     {
         StringBuilder builder = new StringBuilder();
 
-        try (FileInputStream fis = new FileInputStream(ap.getFilePath()))
+        try (FileInputStream fis = new FileInputStream(filePath))
         {
             int ch;
             while ((ch = fis.read()) != -1) { builder.append((char) ch); }
         }
         catch (Exception e) { e.printStackTrace(); }
 
-        System.out.println("1. text : ");
-        System.out.println(builder);
-
         return builder.toString();
     }
 
-    private static Tuple<String, HashMap<Character, String>> readCompressedFile(ArgumentsParser ap)
+    private static Tuple<String, HashMap<Character, String>> readCompressedFile(String filePath)
     {
         String huffmanText = "";
         HashMap<Character, String> charHuffmanMap = new HashMap<>();
 
-        try (FileInputStream fis = new FileInputStream(ap.getFilePath());
+        try (FileInputStream fis = new FileInputStream(filePath);
              ObjectInputStream ois = new ObjectInputStream(fis))
         {
             huffmanText = (String) ois.readObject();
             charHuffmanMap = (HashMap<Character, String>) ois.readObject();
-
-            System.out.println("1. huffmanText : ");
-            System.out.println(huffmanText);
-            System.out.println("1. charHuffmanMap : ");
-            charHuffmanMap.forEach((k,v) -> System.out.println(k + " -> " + v));
         }
         catch (Exception e)
         {
